3b2f64669ce9c2ba3e76e2407908e364929ade17
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-30 14:22:14 +0100

    counter can't go below 0

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,10 @@ const intialState: StateType = {
 const reducer = (state: StateType, action: Action) => {
     switch (action.type) {
         case ActionType.increment:
-            state.count++
-            break
+            return { ...state, count: state.count + 1 }
+
         case ActionType.decrement:
-            state.count--
-            break
+            return { ...state, count: Math.max(0, state.count - 1) }
     }
     return { ...state }
 }
@@ -34,10 +33,28 @@ const reducer = (state: StateType, action: Action) => {
 function App() {
     const [state, dispatch] = useReducer(reducer, intialState)
     return (
-        <div>
-            <h1>Counter</h1>
-            <p>Count: {state.count} </p>
-        </div>
+        <>
+            <div>
+                <h1>Counter</h1>
+                <p>Count: {state.count} </p>
+            </div>
+            <div>
+                <button
+                    onClick={() =>
+                        dispatch({ type: ActionType.increment, payload: 0 })
+                    }
+                >
+                    +
+                </button>
+                <button
+                    onClick={() =>
+                        dispatch({ type: ActionType.decrement, payload: 0 })
+                    }
+                >
+                    -
+                </button>
+            </div>
+        </>
     )
 }
 
