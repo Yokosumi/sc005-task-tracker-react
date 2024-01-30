d1e0bd2cd3ead839960ebaed4695f37d0b3a311f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-30 14:00:28 +0100

    useReducer counter stands

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import { useReducer } from 'react'
 import './index.scss'
 
+type StateType = {
+    count: number
+}
+
+enum ActionType {
+    increment = 'increment',
+    decrement = 'decrement',
+}
+
+type Action = {
+    type: ActionType
+    payload: number
+}
+
+const intialState: StateType = {
+    count: 0,
+}
+
+const reducer = (state: StateType, action: Action) => {
+    switch (action.type) {
+        case ActionType.increment:
+            state.count++
+            break
+        case ActionType.decrement:
+            state.count--
+            break
+    }
+    return { ...state }
+}
+
 function App() {
+    const [state, dispatch] = useReducer(reducer, intialState)
     return (
-        <>
-            <h1 className="text-red-500 text-5xl">Hello</h1>
-        </>
+        <div>
+            <h1>Counter</h1>
+            <p>Count: {state.count} </p>
+        </div>
     )
 }
 
