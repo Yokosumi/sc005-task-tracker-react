f6f4a7993f291f968e69a26dde1e2fdd5ad669e1
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-30 13:41:38 +0100

    minimizes project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-
 import './index.scss'
 
 function App() {
-
-
-  return (
-    <>
-      <h1 className='text-red-500 text-5xl'>Hello</h1>
-      
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-red-500 text-5xl">Hello</h1>
+        </>
+    )
 }
 
 export default App
